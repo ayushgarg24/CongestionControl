50087e3dd00a7ea4d43a9283b2b8246a06f3cdd1
Ayush Garg <ayushgarg1551@gmail.com>
2022-05-16 19:51:51 -0700

    finished stop and wait

--- stopandwait.py
+++ stopandwait.py
@@ -1,6 +1,15 @@
 from socket import *
 import time
+import math
 
+def printInformation(currentWin, seqSent, ackNum):
+    print()
+    print("Current Window: {}".format(currentWin))
+    print("Seqeuence Number of Packet Sent: {}".format(seqSent))
+    print("Acknowledgement Number Received: {}".format(ackNum))
+    print()
+
+    
 # CONSTANTS
 # IP address and port number
 IP_ADDRESS = ""
@@ -14,11 +23,51 @@ FIXED_TIMEOUT = 5
 
 # sender socket
 client_socket = socket(AF_INET, SOCK_DGRAM)
+client_socket.settimeout(FIXED_TIMEOUT)
 filename = 'message.txt'
 
-with open(filename, 'r') as file:
-    while True:
-        # check timeout here and break
+transmitTimes = []
+ackTimes = []
 
-        packet = filename.read(PACKET_SIZE)
+with open(filename, 'r') as file:
+    seq_num = 1
+    while True:
+        packet = file.read(PACKET_SIZE)
+        if not packet:
+            break
+        header = '{}|'.format(seq_num)
+        packet = header + packet
+        
+        t0 = time.time()
         client_socket.sendto(packet.encode(),(IP_ADDRESS, RECEIVING_PORT))
+        transmitTimes.append(t0)
+
+        ACK = False
+        while not ACK:
+            try:
+                response, address = client_socket.recvfrom(2048)
+                ackTime = time.time()
+                ACK = True
+                ackNum = response.decode()
+                ackTimes.append(ackTime)
+                printInformation([seq_num], seq_num, ackNum)
+                seq_num += 1
+                
+            except socket.timeout:
+                client_socket.sendto(packet.encode(),(IP_ADDRESS, RECEIVING_PORT))
+
+delayTimes = []
+throughputs = []
+for i in range(len(transmitTimes)):
+    delay = ackTimes[i] - transmitTimes[i]
+    throughput = 8*1002/delay
+    delayTimes.append(delay)
+    throughputs.append(throughput)
+
+avgDelay = sum(delayTimes)/len(delayTimes) * 1000
+avgThroughput = sum(throughputs)/len(throughputs)
+Performance = math.log10(avgThroughput/avgDelay)
+
+print("Average Delay = {}".format(avgDelay))
+print("Average Throughput = {}".format(avgThroughput))
+print("Performance = {}".format(Performance))
