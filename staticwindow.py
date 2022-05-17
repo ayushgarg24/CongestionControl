79b27ab09cc5d3f99a401dc90c84e1d192d44012
Nathankg7 <92001353+Nathankg7@users.noreply.github.com>
2022-05-16 21:09:45 -0700

    Add files via upload

--- staticwindow.py
+++ staticwindow.py
@@ -2,6 +2,8 @@
 import socket
 import re
 import sys
+import time
+import math
 
 
 # Defining Global Parameters
@@ -24,10 +26,26 @@ ack_num = -1
 curr_ack = -1
 base = 1
 next_seq_num = 1
+temp_end = -1
+delay_sum = 0
+throughput_sum = 0
+delays = [0]
+throughputs = [0]
+#initialize lists for tracking the timing
+times = []
+start = [0,0]
+times.append(start)
 socket.setdefaulttimeout(5)
 
 # Instatiating a UDP Socket
 sender_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+def printInfo(currentWin, seqSent, ackNum):
+    print()
+    print("Current Window:{}".format(currentWin))
+    print("Sequence Number of Packet Sent:{}".format(seqSent))
+    print("Acknowledgement Number Received:{}".format(ackNum))
+    print()
 
 
 #general process: read data from file, add delim to the beginning of the data, to form packet,
@@ -46,25 +64,32 @@ while(True):
         data = f.read(PACKET_SIZE) # read 1000 bytes from file
         if(len(data) == 0): break # if were done reading data then break out of the loop
         data_str = data.decode() # convert to string so we can add header
-        print("data size:", len(data))
+        #print("data size:", len(data))
         if(len(data) < 300): 
             print("data:",data)
             #break
         delim = str(next_seq_num) + '|'
         data_str = delim + data_str  
-        data_packets.append(data_str) #store after we add the header
-        #print("data str:", data_str)
+        data_packets.append(data_str) #store packet after we add the header
         print("sending packet:", next_seq_num)
+        start_time = time.time() #start time here
         sender_socket.sendto(data_str.encode(), (IP_ADDRESS, PORT))
+        start = [start_time]
+        times.append(start)
         next_seq_num = next_seq_num + 1
         continue
         #receiver acknowledgement
     try:
         prev_ack = ack_num 
         ack_num,addr = sender_socket.recvfrom(4)  
-        #curr_ack = ack_num
+        ack_num = int(ack_num)
+        #print("ack num:",ack_num)
         if(ack_num == prev_ack): #Check for same ack number back to back
-            sender_socket.sendto(data_packets[ack_num + 1].encode(), (IP_ADDRESS, PORT))
+            sender_socket.sendto(data_packets[ack_num + 1].encode(), (IP_ADDRESS, PORT)) #resend packet
+        else: #We know its not a duplicate ack, so save the end time
+            #print("ack num:",ack_num)
+            end_time = time.time()
+            times[ack_num].append(end_time)
         #print("ack size:", len(ack_num))
         #print("got ack for packet:", ack_num.decode())
 
@@ -77,4 +102,53 @@ while(True):
         sender_socket.sendto(data_packets[ack_num + 1].encode(), (IP_ADDRESS, PORT))
         break
 
+#get the leftover acks
+while(ack_num < next_seq_num - 1):
+    try:
+        prev_ack = ack_num 
+        ack_num,addr = sender_socket.recvfrom(4)  
+        ack_num = int(ack_num)
+        if(ack_num == prev_ack): #Check for same ack number back to back
+            sender_socket.sendto(data_packets[ack_num + 1].encode(), (IP_ADDRESS, PORT)) #resend packet
+        else: #We know its not a duplicate ack, so save the end time
+            #print("ack num:",ack_num)
+            end_time = time.time()
+            times[ack_num].append(end_time)
+        #print("ack size:", len(ack_num))
+        #print("got ack for packet:", ack_num.decode())
+
+        #When we get an acknowledgement, we know all packets up to that number have been acknowledged
+        #so we can make the base that acknowledgement
+        base = int(ack_num)
+        continue
+    except socket.timeout:
+        print("had a timeout")
+        sender_socket.sendto(data_packets[ack_num + 1].encode(), (IP_ADDRESS, PORT))
+        break
+
+#Add end times to the ones that dont have end times
+for x in reversed(times):
+    if(len(x) == 2):
+        temp_end = x[1]
+    else:
+        x.append(temp_end)
+
+#calculate avg delay throughput and performance
+for x in times:
+    delays.append(x[1] - x[0])
+
+for x in delays:
+    delay_sum += x
+    if x != 0:
+        throughputs.append((PACKET_SIZE + 2)/ x)
+
+for x in throughputs:
+    throughput_sum += x
+
+avg_delay = (delay_sum / (len(delays) - 1)) * 1000 #avg delay in ms
+avg_throughput = (throughput_sum / len(throughputs) - 1) * 8 #avg throughput in bits per second
+performance = math.log10(avg_throughput) - math.log10(avg_delay)
+print("Average Delay =", avg_delay, "ms")
+print("Average Throughput =", avg_throughput, "bps")
+print("Performance =", performance)
 sender_socket.close()
